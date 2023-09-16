bf65b1c41d66f96fc6b5a0fd0ec27fa0ecd10704
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-09-16 13:14:13 +0530

    something removed

--- array.java
+++ array.java
@@ -1,7 +1,6 @@
 class array{
 	public static void main(String[] args){
 		int arr[];
-		String str = "test";
 		arr = new int[5];
 		for(int i=0; i<arr.length; i++) arr[i] = i+1;
 		for(int i: arr) System.out.print(i+" ");
